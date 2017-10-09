f7326168f74ab6243b996423cea4a555818c1f04
Olle Raty <olle.raty@helsinki.fi>
2017-10-09 09:17:25 +0000

    working version of calc_cmpi.r

--- R-scripts/calc_cmpi.r
+++ R-scripts/calc_cmpi.r
@@ -1,5 +1,28 @@
 library(DECM)
-source("/home/ubuntu/abc4cde_wp4/back-end/R/") #Did not manage to install the latesr version of DECM!
+source("/home/ubuntu/abc4cde_wp4/back-end/R/get.R") #Did not manage to install the latest version of DECM!
+source("/home/ubuntu/abc4cde_wp4/back-end/R/regions.R") #Did not manage to install the latest version of DECM!
+
+get_scriptpath <- function() {
+  # location of script can depend on how it was invoked:
+  # source() and knit() put it in sys.calls()
+  path <- NULL
+  if(Sys.getenv("RSTUDIO") == "1"){
+    path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  }else if(!is.null(sys.calls())) {
+    # get name of script - hope this is consisitent!
+    path <- as.character(sys.call(1))[2] 
+    # make sure we got a file that ends in .R, .Rmd or .Rnw
+    if (grepl("..+\\.[R|Rmd|Rnw]", path, perl=TRUE, ignore.case = TRUE) )  {
+      return(path)
+    } else { 
+      message("Obtained value for path does not end with .R, .Rmd or .Rnw: ", path)
+    }
+  } else{
+    # Rscript and R -f put it in commandArgs
+    args <- commandArgs(trailingOnly = FALSE)
+  }
+  return(path)
+}
 
 #Calculate weights for the weighted average of rms, assuming monthly time step.
 calc.mon.weights <- function(lon,lat){
@@ -7,10 +30,9 @@ calc.mon.weights <- function(lon,lat){
   weights <- array(NA,dim=c(12,length(lon),length(lat)))
   time.weights <- c(31,28,31,30,31,30,31,31,30,31,30,31)/365
   lat.weights <- rep(cos(pi*lat/180),length(lon))
-  dim(lat.weights) <- c(length(lon),length(lat))
-  lon.weights <- rep(1,length(lon)*length(lat))
-  dim(lon.weights) <- c(length(lon),length(lat))
-  
+  dim(lat.weights) <- c(length(lat),length(lon))
+  lat.weights <- t(lat.weights)
+  image(lat.weights)
   for(i in 1:length(lat)){
     weights[,,i] <- time.weights
   }
@@ -22,53 +44,85 @@ calc.mon.weights <- function(lon,lat){
   return(weights)
 }
 
-period <- c(1981,2010)
-shape <-  get.shapefile("/home/ubuntu/abc4cde_wp4/back-end/data/SREX_regions/referenceRegions.shp",with.path = T)
- getGCMs(select=1:4,varid="pr")
- getGCMs(select=1:4,varid="tas")
+calculate.cmpi <- function(reference="era",period=c(1981,2010), variable="tas", nfiles=4,
+                                      continue=F, verbose=F){
 
-vars <- c("tas","pr")
-
-#Pre-process ERA-interim
-for(var in vars){
-  era.file <- paste("/home/ubuntu/Data/Data/era-interim_monthly_1979-2017_",var,".2.5deg.nc",sep="")
-  era.mulc <- paste("/home/ubuntu/Data/Data/era.mulc",var,"nc",sep=".")
-  era.mon.file <- paste("/home/ubuntu/Data/Data/era.monmean",var,"nc",sep=".")
-  if(var=="pr"){
-    cdo.command("mulc",1000,era.file,era.mulc)
+  shape <-  get.shapefile("../back-end/data/SREX_regions/referenceRegions.shp",with.path = T)
+  external <- "/home/ubuntu/Data/Data/" #COMPUTER SPECIFIC, CHANGE!!!!!
+    
+  store <- list()
+  store.file <- paste("statistics.cmip", reference, variable, paste(period, collapse="-"), "rda", sep=".")
+  if(file.exists(store.file)) load(store.file)
+  
+  #Pre-process ERA-interim, if necessary
+  era.file <- paste(external,"era-interim_monthly_1979-2017_",variable,".2.5deg.nc",sep="")
+  era.mulc <- paste(paste(external,"era.mulc",sep=""),variable,"nc",sep=".")
+  era.mon.file <- paste(paste(external,"era.monmean",sep=""),variable,"nc",sep=".")
+  
+  if(variable=="pr"){
+    if(!file.exists(era.mulc)) cdo.command("mulc",1000,era.file,era.mulc)
   }else{
-    era.mulc <- era.file
+    if(!file.exists(era.mulc)) era.mulc <- era.file
   }
-  cdo.command(c("-ymonmean","-selyear"),c("",paste(period,collapse="/")),
-              infile=era.mulc,outfile=era.mon.file)
-}
-
-#Calculate weights only once
-r <- raster(era.mon.file)
-lon <- xFromCol(r)
-lat <- yFromRow(r)
-weights <- calc.mon.weights(lon,lat)
-
-#For testing
-vars <- c("tas","pr")
-rms <- e <- list()
-models <- 1:4
-for(var in vars){
-  era.mon.file <- paste("/home/ubuntu/Data/Data/era.monmean",var,"nc",sep=".")
+  
+  if(!file.exists(era.mon.file)) cdo.command(c("-ymonmean","-selyear"),c("",paste(period,collapse="/")),
+             infile=era.mulc,outfile=era.mon.file)
   era <- coredata(retrieve(era.mon.file))
-  dim(era) <- c(12,length(longitude(era)),length(latitude(era)))
-  for(i in models){
-    gcm.file <- paste(paste("/home/ubuntu/Data/Data/GCM",i,sep=""),var,"nc",sep=".")
-    gcm.mon.file <- "/home/ubuntu/Data/Data/gcm.monmean.nc"
+  
+  #Calculate weights only once
+  r <- raster(era.mon.file)
+  lon <- xFromCol(r)
+  lat <- yFromRow(r)
+  weights <- calc.mon.weights(lon,lat)
+  
+  #Check which files are processed
+  ngcm <- length(cmip5.urls(varid=variable))
+  start <- 1
+  if(continue && file.exists(store.file))
+    start <- as.numeric(tail(sub('.*\\.', '', names(store), perl=T),n=1))+1
+  if(nfiles=="all"){
+    end <- ngcm
+  }else{
+    end <- min(start+nfiles-1,ngcm) 
+  }
+
+  for(i in start:end){
+    store.name <- paste("gcm",i,sep=".")
+    gcm.file <- paste(paste(external,"GCM",i,sep=""),variable,"nc",sep=".")
+    if(!file.exists(gcm.file))getGCMs(i,varid=variable,destfile=paste(external,gcm.file,sep=""))
+    gcm.mon.file <- paste(external,"gcm.monmean.nc",sep="")
     cdo.command(c("-ymonmean","-selyear"),c("",paste(period,collapse="/")),
                 infile=gcm.file,outfile=gcm.mon.file)
     gcm <- coredata(retrieve(gcm.mon.file))
-    dim(gcm) <- c(12,length(longitude(gcm)),length(latitude(gcm)))
-    rms[[var]][[i]] <- sqrt(sum(weights*(gcm-era)^2)/sum(weights))
+    dim(gcm) <- dim(era) <- c(12,length(longitude(gcm)),length(latitude(gcm)))
+    store[[store.name]]$rms <- sqrt(sum(weights*(gcm-era)^2)/sum(weights))
+    for(region in levels(shape$LAB)){
+      polygon <- shape[which(levels(shape$LAB)==region),]
+      mask <- gen.mask.srex(destfile=gcm.file,mask.polygon=polygon)
+      dim(gcm) <- dim(era) <- c(12,length(longitude(era))*length(latitude(era)))
+      gcm.masked <- mask.zoo(gcm,mask)
+      era.masked <- mask.zoo(era,mask)
+      dim(gcm.masked) <- dim(era.masked) <- c(12,length(longitude(gcm)),length(latitude(gcm)))
+      store[[store.name]][[region]]$rms <- sqrt(sum(weights*(gcm.masked-era.masked)^2,na.rm=T)/sum(weights[!is.na(gcm.masked)]))
+    }
+    file.remove(gcm.mon.file)
   }
   
-  rms[[var]][["median"]] <- median(unlist(rms[[var]]))
-  for(i in 1:4){
-    e[[var]][[i]] <- (rms[[var]][[i]]-rms[[var]][["median"]])/rms[[var]][["median"]]
+  median.rms <- median(unlist(lapply(store, "[","rms")))  
+  for(i in start:end){
+    store[[store.name]]$e <- (store[[store.name]]$rms-median.rms)/median.rms
+    for(region in levels(shape$LAB)){
+      median.rms <- median(unlist(lapply(lapply(store, "[",region),function(x) x[[region]]$rms)))
+      store[[store.name]][[region]]$e <- (store[[store.name]][[region]]$rms-median.rms)/median.rms
+    }
   }
+  save(file=store.file,store)
+  file.remove(era.mon.file)
 }
+
+period <- c(1981,2010)
+setwd(get_scriptpath())
+variable <- "tas"
+nfiles <- 4
+calculate.cmpi(period=c(1981,2010), variable="tas", nfiles=4,
+                           continue=F, verbose=F)
--- back-end/R/get.R
+++ back-end/R/get.R
@@ -1,6 +1,6 @@
 #Helper function to find files linux environment (add Windows counterpart)
 find.file <- function(filename) {
-  command <- paste("find -L $HOME -name",filename,sep=" ")
+  command <- paste("find -L $HOME/abc4cde_wp4/back-end/data -name",filename,sep=" ")
   fullpath <- system(command,intern=TRUE)
   if(length(fullpath)==0) return(FALSE)
   return(fullpath)
